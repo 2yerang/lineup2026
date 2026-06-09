2183199e0fbbccb7702ade05002ede273835260b
2yerang <leryearang@gmail.com>
2026-06-09 14:17:55 +0900

    Initial commit: 2026 뮤지컬 공연 라인업 Gantt 차트

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'lineup2026-v1';
+const BASE = '/lineup2026';
+const ASSETS = [BASE + '/', BASE + '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
